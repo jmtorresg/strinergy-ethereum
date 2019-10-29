2b23d2da294bfec04911df837b7be32f1da16e3f
Juan Manuel Torres García <jmtorresg@gmx.com>
2019-10-29 10:22:01 +0100

    Commented tests

--- test/strinergy.js
+++ test/strinergy.js
@@ -3,21 +3,33 @@ const AccessToken = artifacts.require("AccessToken");
 
 contract("When distributing production", accounts => {
     it('should distribute profits correctly', async () => {
+        // Deploy the contracts
         const AT = await AccessToken.deployed();
         const ET = await EnergyToken.deployed();
-        const project = AT.address;
-        const meter = accounts[0];
-        const participant1 = accounts[1];
-        const participant2 = accounts[2];
-        const participant3 = accounts[3];
 
-        await AT.transfer(participant1, 4500);
-        await AT.transfer(participant2, 4500);
+        // Define the actors
+        const project = AT.address;
+        const owner = accounts[0];
+        const meter = accounts[1];
+        const participant1 = accounts[2];
+        const participant2 = accounts[3];
+        const participant3 = accounts[4];
+
+        // Transfer 4500 AT to each of the first two participants
+        await AT.transfer(participant1, 4500, {from: owner});
+        await AT.transfer(participant2, 4500, {from: owner});
+
+        // Transfer 4500 AT from participant 2 to participant 3
         await AT.transfer(participant3, 4500, {from: participant2})
+
+        // Register the project and the meter
         await ET.registerProject(project);
         await ET.registerMeter(meter, project);
+
+        // Emit 20546 tokens
         await ET.productionNotify(20546, {from: meter});
 
+        // Test Access Token balances
         const ATBalance1 = (await AT.balanceOf(participant1)).toNumber();
         const ATBalance2 = (await AT.balanceOf(participant2)).toNumber();
         const ATBalance3 = (await AT.balanceOf(participant3)).toNumber();
@@ -25,6 +37,7 @@ contract("When distributing production", accounts => {
         expect(ATBalance2).to.equal(0);
         expect(ATBalance3).to.equal(4500);
         
+        // Test Energy Token balances
         const ETBalance1 = (await ET.balanceOf(participant1)).toNumber();
         const ETBalance2 = (await ET.balanceOf(participant2)).toNumber();
         const ETBalance3 = (await ET.balanceOf(participant3)).toNumber();
